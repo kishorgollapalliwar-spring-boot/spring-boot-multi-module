388f3919918118f89c8987c50d948e041f32f791
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-19 11:26:53 +0530

    e-comm: [web] Changed base package to scan

--- e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
+++ e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
@@ -3,7 +3,7 @@ package com.kishor.home.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.kishor.home"})
 public class WebApplication {
 
 	public static void main(String[] args) {
