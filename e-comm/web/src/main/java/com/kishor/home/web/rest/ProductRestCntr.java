e052d5addd9ce001da95a959d885ad604f1a22c4
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-21 21:02:12 +0530

    e-comm: [web] Added Product Rest Controller

---
+++ e-comm/web/src/main/java/com/kishor/home/web/rest/ProductRestCntr.java
@@ -0,0 +1,22 @@
+package com.kishor.home.web.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kishor.home.core.entity.ProductEnt;
+import com.kishor.home.core.service.ProductService;
+
+@RestController
+@RequestMapping(path = {"/product"})
+public class ProductRestCntr {
+	@Autowired private ProductService productService;
+
+	@GetMapping("/list")
+	public List<ProductEnt> list() {
+		return productService.list();
+	}
+}
