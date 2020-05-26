d888dd22692b5aa64d048f1ec0dcf7978853f109
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-26 12:47:32 +0530

    e-comm: [core] Corrected unique key constraint in product entity

--- e-comm/core/src/main/java/com/kishor/home/core/entity/ProductEnt.java
+++ e-comm/core/src/main/java/com/kishor/home/core/entity/ProductEnt.java
@@ -1,22 +1,24 @@
 package com.kishor.home.core.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(uniqueConstraints = {
+		@UniqueConstraint(columnNames={"name", "brand"})
+})
 public class ProductEnt {
 	@Id
 	@GeneratedValue
 	private Integer id;
 
-	@Column(unique=true)
 	private String name;
-
 	private String brand;
 	private String manufacturer;
 }
