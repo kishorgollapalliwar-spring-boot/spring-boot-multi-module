73dfe3882d83d7c335e4b622917482a30208006e
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-27 13:20:49 +0530

    e-comm: [core] Fixed product unique key issue in product repo

--- e-comm/core/src/main/java/com/kishor/home/core/repo/ProductRepo.java
+++ e-comm/core/src/main/java/com/kishor/home/core/repo/ProductRepo.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.query.Param;
 import com.kishor.home.core.entity.ProductEnt;
 
 public interface ProductRepo extends JpaRepository<ProductEnt, Integer> {
-	public ProductEnt findByName(@Param("name") final String name);
+	public ProductEnt findByNameAndBrand(@Param("name") final String name, @Param("brand") final String brand);
 }
