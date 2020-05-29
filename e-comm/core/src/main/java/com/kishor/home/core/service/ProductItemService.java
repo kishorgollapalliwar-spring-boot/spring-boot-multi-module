7a39bcf88985d722f3ce19b7f492085887aff3c6
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-29 08:54:27 +0530

    e-comm: [core] Added product item service

---
+++ e-comm/core/src/main/java/com/kishor/home/core/service/ProductItemService.java
@@ -0,0 +1,35 @@
+package com.kishor.home.core.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kishor.home.core.entity.ProductItemEnt;
+import com.kishor.home.core.repo.ProductItemRepo;
+
+@Service
+public class ProductItemService {
+	private ProductItemRepo productItemRepo;
+
+	@Autowired
+	public ProductItemService(ProductItemRepo productItemRepo) {
+		this.productItemRepo = productItemRepo;
+	}
+
+	public List<ProductItemEnt> list() {
+		return productItemRepo.findAll();
+	}
+
+	public ProductItemEnt create(final ProductItemEnt productItemEnt) {
+		return productItemRepo.save(productItemEnt);
+	}
+
+	public void delete(final ProductItemEnt productItemEnt) {
+		productItemRepo.delete(productItemEnt);
+	}
+
+	public Long totalProductItems() {
+		return productItemRepo.count();
+	}
+}
