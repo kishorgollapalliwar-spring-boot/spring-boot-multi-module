c676b2b50cda358852b8996be946e008e5129271
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-05-31 10:07:16 +0530

    e-comm: [core] Added Search By Product method to ProductItem Repository

--- e-comm/core/src/main/java/com/kishor/home/core/repo/ProductItemRepo.java
+++ e-comm/core/src/main/java/com/kishor/home/core/repo/ProductItemRepo.java
@@ -1,9 +1,13 @@
 package com.kishor.home.core.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.kishor.home.core.entity.ProductEnt;
 import com.kishor.home.core.entity.ProductItemEnt;
 
 public interface ProductItemRepo extends JpaRepository<ProductItemEnt, Integer> {
-
+	public List<ProductItemEnt> findByProduct(@Param("product") final ProductEnt product);
 }
