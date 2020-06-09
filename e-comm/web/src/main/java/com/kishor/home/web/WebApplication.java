bb704d430c4ced887fe606fe090ba179a4955751
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-06-09 20:57:26 +0530

    e-comm: [web] Added Model mapper bean to spring container

--- e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
+++ e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
@@ -1,7 +1,9 @@
 package com.kishor.home.web;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.kishor.home"})
 public class WebApplication {
@@ -10,4 +12,8 @@ public class WebApplication {
 		SpringApplication.run(WebApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
