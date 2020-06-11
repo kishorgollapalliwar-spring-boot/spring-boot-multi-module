080b21246629ebdfaea237e14c47230eeb5aa204
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-06-11 06:18:41 +0530

    e-comm: [core] ModelMapper changes in ProductDTO

--- e-comm/core/src/main/java/com/kishor/home/core/dto/ProductDTO.java
+++ e-comm/core/src/main/java/com/kishor/home/core/dto/ProductDTO.java
@@ -1,20 +1,21 @@
 package com.kishor.home.core.dto;
 
+import org.modelmapper.ModelMapper;
+
 import com.kishor.home.core.entity.ProductEnt;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class ProductDTO {
+@NoArgsConstructor
+public class ProductDTO implements BaseDTO {
 	private Integer id;
 	private String name;
 	private String brand;
 	private String manufacturer;
 
-	public ProductDTO(final ProductEnt productEnt) {
-		this.id = productEnt.getId();
-		this.name = productEnt.getName();
-		this.brand = productEnt.getBrand();
-		this.manufacturer = productEnt.getManufacturer();
+	public ProductDTO(final ModelMapper modelMapper, final ProductEnt productEnt) {
+		this.setDto(modelMapper, productEnt);
 	}
 }
