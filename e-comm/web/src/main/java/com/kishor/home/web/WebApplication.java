ee31bd06346570af2e75e5dbd2ea2cf7eeeff4d3
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-06-14 06:54:36 +0530

    e-comm: [web] ModelMapper changes removed from WebApplication

--- e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
+++ e-comm/web/src/main/java/com/kishor/home/web/WebApplication.java
@@ -1,19 +1,12 @@
 package com.kishor.home.web;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.kishor.home"})
 public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 }
