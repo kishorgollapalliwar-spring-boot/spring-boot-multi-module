998060124011b3debab76ac0b56221a99c531d73
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-06-20 08:27:14 +0530

    e-comm: [core] ProductRest - Updated list method to return DTOs

--- e-comm/web/src/main/java/com/kishor/home/web/rest/ProductRestCntr.java
+++ e-comm/web/src/main/java/com/kishor/home/web/rest/ProductRestCntr.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kishor.home.core.dto.ProductDTO;
-import com.kishor.home.core.entity.ProductEnt;
 import com.kishor.home.core.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +20,7 @@ public class ProductRestCntr {
 	@Autowired private ProductService productService;
 
 	@GetMapping(path = {"/", "/list"})
-	public List<ProductEnt> list() {
+	public List<ProductDTO> list() {
 		return productService.list();
 	}
 
