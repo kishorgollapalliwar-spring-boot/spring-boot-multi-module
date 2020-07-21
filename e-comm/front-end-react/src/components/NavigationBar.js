3e8b7e71061a74b0995baf554f4dcfbddceb0fd7
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-07-21 18:55:41 +0530

    e-comm: [UI] Added Navigation Bar component

---
+++ e-comm/front-end-react/src/components/NavigationBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+	render() {
+		return (
+			<div>NavigationBar</div>
+		);
+	}
+}
+
+export default NavigationBar;
+/*
+npx create-react-app front-end-react
+npm install react-bootstrap bootstrap
+index.html => <link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+  crossorigin="anonymous"
+/>
+*/
