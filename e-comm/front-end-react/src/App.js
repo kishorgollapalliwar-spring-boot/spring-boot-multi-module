7df50b72029299f5de54ada572b9123baba38689
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-07-22 18:57:03 +0530

    e-comm: [UI] Updated App component with Navigation Bar changes.

--- e-comm/front-end-react/src/App.js
+++ e-comm/front-end-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar'
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
