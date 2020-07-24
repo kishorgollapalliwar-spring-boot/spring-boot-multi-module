5ba5e30687062d1695d6761d9f917b8238b10a91
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-07-24 19:05:45 +0530

    e-comm: [UI] NavigationBar - Removed commented code.

--- e-comm/front-end-react/src/components/NavigationBar.js
+++ e-comm/front-end-react/src/components/NavigationBar.js
@@ -9,13 +9,3 @@ class NavigationBar extends React.Component {
 }
 
 export default NavigationBar;
-/*
-npx create-react-app front-end-react
-npm install react-bootstrap bootstrap
-index.html => <link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-  crossorigin="anonymous"
-/>
-*/
