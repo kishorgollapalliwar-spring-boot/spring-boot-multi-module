200df8c406c0edaeb1c9191c5a5375f286c0ad5c
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-07-26 20:02:51 +0530

    e-comm: [UI] NavigationBar - Used home component.

--- e-comm/front-end-react/src/App.js
+++ e-comm/front-end-react/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavigationBar from './components/NavigationBar'
+import NavigationBar from './components/NavigationBar';
+import Home from './components/Home';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
+  const marginTop = {
+    marginTop:"2.5%"
+  };
+
   return (
     <div className="App">
       <NavigationBar />
+      <Container>
+        <Row>
+          <Col lg={12} style={marginTop}>
+            <Home />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
