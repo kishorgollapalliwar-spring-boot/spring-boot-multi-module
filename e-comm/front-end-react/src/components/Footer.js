94aa0f772d932d2142d35b9bfaa440f00adb0a38
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-07-29 09:00:29 +0530

    e-comm: [UI] Footer - Created footer component.

---
+++ e-comm/front-end-react/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
+
+class Footer extends React.Component {
+	render() {
+		let currYear = new Date().getFullYear();
+		return (
+			<Navbar fixed="bottom" bg="dark" variant="dark">
+				<Container>
+					<Row>
+						<Col lg={12} className="text-center text-muted">
+							<div>{currYear}-{currYear+1}, All Rights Reserved by React App E-Comm.</div>
+						</Col>
+					</Row>
+				</Container>
+			</Navbar>
+		);
+	}
+}
+
+export default Footer;
