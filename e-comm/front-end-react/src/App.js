d0b32d0440af23ad0ded041328c7f90d331b6cb6
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-08-01 10:31:24 +0530

    e-comm: [UI] App - Product component changes.

--- e-comm/front-end-react/src/App.js
+++ e-comm/front-end-react/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavigationBar from './components/NavigationBar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Product from './components/Product';
 import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Row>
           <Col lg={12} style={marginTop}>
             <Home />
+            <Product />
           </Col>
         </Row>
       </Container>
