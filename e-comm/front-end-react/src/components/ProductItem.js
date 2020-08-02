afd2058b6a8e00591096eb754d22a1538d4dfbdd
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2020-08-02 10:32:40 +0530

    e-comm: [UI] ProductItem - Added component.

---
+++ e-comm/front-end-react/src/components/ProductItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ProductItem extends React.Component {
+	render() {
+		return (
+			<div className="text-white">Product Item</div>
+		);
+	}
+}
+export default ProductItem;
